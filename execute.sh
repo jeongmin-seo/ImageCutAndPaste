670aedc331c008a8108decc02a6791c0526b2aef
ya4615 <ya4615@naver.com>
2019-07-16 22:21:37 +0900

    modified shell script

--- execute.sh
+++ execute.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-python cut_image.py --image_name './img/cat_img.jpg' --num_col 2 --num_row 2 --output_dir './'
-python paste_image.py --input_dir './cut_image' --num_col 2 --num_row 2 --output_dir './'
+
+for row in 2 3 4
+do
+    for col in 2 3 4
+    do
+        mkdir "${row}_${col}"
+        python cut_image.py --image_name './img/cat_img.jpg' --num_col ${col} --num_row ${row} --output_dir "./${row}_${col}"
+        python paste_image.py --input_dir "./${row}_${col}/cut_image" --num_col ${col} --num_row ${row} --output_dir "./${row}_${col}"
+    done
+done
